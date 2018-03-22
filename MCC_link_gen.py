f87b282e3b49d43db07f0d2ec8e38ca80d19e050
miha9 <kerc.miha@gmail.com>
2018-03-22 20:46:57 +0100

    Update MCC_link_gen.py

--- MCC_link_gen.py
+++ MCC_link_gen.py
@@ -6,13 +6,11 @@ bina = ccxt.binance()
 markets_dic = {}
 tickers = bina.fetch_tickers()
 link = "https://www.multicoincharts.com/"
-#print(tickers)
 for ticker in tickers:
     if ticker.find("/BTC") != -1:
         #print(ticker, tickers[ticker]["quoteVolume"])
         markets_dic[ticker] = tickers[ticker]["quoteVolume"]
 sorted_markets = sorted(markets_dic.items(),  key= operator.itemgetter(1), reverse=True)
-print(sorted_markets)
 for market in range(n_coins):
     #print(sorted_markets[market][0])
     temp_str = (sorted_markets[market][0]).replace("/", "")
